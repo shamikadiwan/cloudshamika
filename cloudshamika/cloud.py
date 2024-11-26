815d68c8f20af16157dd684adf2c96b42bea92ee
shamikadiwan <96711874+shamikadiwan@users.noreply.github.com>
2024-11-26 11:49:38 +0530

    Update cloud.py

--- cloudshamika/cloud.py
+++ cloudshamika/cloud.py
@@ -1,7 +1,8 @@
-total =0
+total = 0
 
 for i in range(10):
-  number =float(input(f"Enter number {i+1}:"))
-  total+=number
+  number = float(input(f"Enter number {i+1}: "))
+  total += number
 
-print(f"Total number of 10 numbers is:{total}")
+print(f"Total number of 10 numbers is: {total}")
+
